db696b79f86d2216012c87c32e915686e52c0181
Jo Cropper <joseph.cropper@bsu.edu>
2025-01-21 09:52:08 -0500

    Update code.c

--- code.c
+++ code.c
@@ -1,5 +1,25 @@
+#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-  return 0;
+    srand(time(NULL)); 
+    int rng = rand() % 100;
+    int guess;
+    while (1){
+        printf("Guess the number: ");
+        scanf("%d", &guess);
+        if (guess == rng){
+            printf("Got it! Number was: %d", rng);
+            return 0;
+        }
+        else if (guess > rng){
+            printf("Too High!\n");
+        }
+        else{
+            printf("Too Low!\n");
+        }
+    }
+    return 0;
 }
+
